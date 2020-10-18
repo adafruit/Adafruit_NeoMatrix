b174f8a49e0dcaeffdd59528aefcb61ecdb68c2d
Marc MERLIN <marc_soft@merlins.org>
2020-10-18 14:38:02 -0700

    Fixed pgmspace on other CPUs.

--- examples/MatrixGFXDemo/linux32.h
+++ examples/MatrixGFXDemo/linux32.h
@@ -4,7 +4,13 @@
 // Image Size     : 32x32 pixels
 // Memory usage   : 2048 bytes
 
+#if defined(__AVR__)
 #include <avr/pgmspace.h>
+#elif defined(__PIC32MX__)
+#define PROGMEM
+#elif defined(__arm__)
+#define PROGMEM
+#endif
 
 const unsigned short bitmap32[1024] PROGMEM={
 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,   // 0x0010 (16) pixels
